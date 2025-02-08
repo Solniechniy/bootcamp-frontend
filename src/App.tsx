9a8c9adfff0b973d8e2f3d973afe2662f02abdb4
Solniechniy <oleksndrkorn@gmail.com>
2025-02-08 13:36:19 +0200

    add first task

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useWallet } from '@solana/wallet-adapter-react';
+import './App.css';
+import logo from '@/assets/logo.svg';
+import ConnectedView from './components/ConectedView';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { publicKey } = useWallet();
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {publicKey ? <ConnectedView /> : <img src={logo} />}
+      {!publicKey ? (
+        <h1>Welcome to Solana FrontEND Bootcamp</h1>
+      ) : (
+        <>
+          <h1>Copy your public key and send to the classroom</h1>
+          <h3>{publicKey.toString()}</h3>
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
 
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import { checker } from 'vite-plugin-checker';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    svgr(),
+    nodePolyfills(),
+    tsconfigPaths({ projects: ['./'] }),
+    checker({
+      typescript: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
